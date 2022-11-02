d38d6864ba3397583b49e276741f5966448f4bc2
Ankur Kayal <ankurkayal1234@gmail.com>
2022-11-03 01:02:15 +0530

    [session#7] Create schema and resolver for fetching a cart by ID

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
@@ -7,17 +7,41 @@ import { PrismaClient } from '@prisma/client';
 // https://pris.ly/d/help/next-js-best-practices
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient<Prisma.PrismaClientOptions, 'query'> | undefined;
 }
 
-let prisma: PrismaClient;
+let prisma: PrismaClient<Prisma.PrismaClientOptions, 'query'>;
 
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    global.prisma = new PrismaClient<Prisma.PrismaClientOptions, 'query'>({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+        {
+          emit: 'stdout',
+          level: 'error',
+        },
+        {
+          emit: 'stdout',
+          level: 'info',
+        },
+        {
+          emit: 'stdout',
+          level: 'warn',
+        },
+      ],
+    });
   }
   prisma = global.prisma;
+  prisma.$on('query', (e) => {
+    console.log('Query: ' + e.query);
+    console.log('Params: ' + e.params);
+    console.log('Duration: ' + e.duration + 'ms');
+  });
 }
 export default prisma;
